66b8cf7639d7c556c9ec36fd1f5e50ce6fe43c37
gpiopi <gpiopi@sina.com>
2018-01-09 11:24:27 +0800

    up

--- main.py
+++ main.py
@@ -10,8 +10,29 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/ocr')
+def ocr():
+    return render_template('ocr.html')
+@app.route('/face')
+def face():
+    return render_template('face.html')
+@app.route('/questions')
+def questions():
+    return render_template('questions.html')
+@app.route('/topics')
+def topics():
+    return render_template('topics.html')
+
 @app.route('/board/<board_name>')
 def board(board_name):
-    return 'board:%s' % (board_name)
+    return render_template('/board/%s.html' % (board_name))
 if __name__=='__main__':
     app.run('0.0.0.0')
